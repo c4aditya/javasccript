01a118fc542417e1055fb7112320d5367ed69503
Aditya Singh <150081139+c4aditya@users.noreply.github.com>
2024-07-22 21:25:32 +0000

    variable done

---
+++ variablejs/variable.js
@@ -0,0 +1,39 @@
+// Variables and constant 
+// declare constant 
+const accountid =144553
+//accountid = 2 change is  not allowed beacuse it is the constant
+
+// let 
+ let accountemail= "adityasogh@gmail.com"
+
+ // var
+
+ var accountpass = "123456"
+
+ // without variable we can also store the value in js 
+
+ accountcity= "ghazipur" 
+
+ // undefined value 
+
+ // if we create the variiable and we do not declare any vlaue then it return undefined
+ 
+ let accountadd 
+
+// chage the value of the let 
+
+accountemail="adirya@gmail.com"
+
+// change the value in without variable name 
+accountcity="delhi"
+
+// there are two ways for print the values 
+
+//1
+console.log();
+
+//2
+console.table([accountid,accountemail,accountpass, accountcity,accountadd])
+/*
+prefer do not use var beacuase it is a create a issue for the scope
+*/
